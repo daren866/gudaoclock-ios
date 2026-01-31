277e15c849408b107b8b2ec2276c9a6d68130f5a
daren866 <105366647+daren866@users.noreply.github.com>
2026-01-31 20:58:04 +0800

    新增对横竖屏的支持

--- GudaoClock/ContentView.swift
+++ GudaoClock/ContentView.swift
@@ -8,18 +8,20 @@ struct ContentView: View {
             Color.black
                 .edgesIgnoringSafeArea(.all)
             
-            VStack(spacing: 40) {
-                // 日期显示
-                Text(viewModel.dateString)
-                    .font(.system(size: 36, weight: .medium))
-                    .foregroundColor(.white)
-                
-                // 时间显示
-                Text(viewModel.timeString)
-                    .font(.system(size: 72, weight: .bold))
-                    .foregroundColor(.white)
+            GeometryReader { geometry in
+                VStack(spacing: geometry.size.width > geometry.size.height ? 30 : 40) {
+                    // 日期显示
+                    Text(viewModel.dateString)
+                        .font(.system(size: geometry.size.width > geometry.size.height ? 40 : 36, weight: .medium))
+                        .foregroundColor(.white)
+                    
+                    // 时间显示
+                    Text(viewModel.timeString)
+                        .font(.system(size: geometry.size.width > geometry.size.height ? 100 : 72, weight: .bold))
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         }
     }
 }
